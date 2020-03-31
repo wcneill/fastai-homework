e91ad9adf78b74db9967a56703073df7b2ae370a
Wesley Neill <wesley.neill@gmail.com>
2020-03-31 13:08:42 -0700

    changed over classifier from lesson template to my homework case

--- Lesson2-v3/l02_deploy/app/server.py
+++ Lesson2-v3/l02_deploy/app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = "https://drive.google.com/open?id=1-ElWkiyqojkAexjztrb1I4xn4h2C_Iw-"
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['butternut', 'pumpkin', 'acorn', 'zucchini', 'tromboncino']
 path = Path(__file__).parent
 
 app = Starlette()
