674b2f9e13d5800860b9eaba0aa0468a9393ba1d
Wesley Neill <wesley.neill@gmail.com>
2020-04-13 08:58:21 -0700

    updated image feed to inferencer

--- Lesson2-v3/inference.py
+++ Lesson2-v3/inference.py
@@ -1,6 +1,7 @@
 import os
 import PIL.Image
 from fastai.vision import *
+import torchvision.transforms as T
 import streamlit as st
 
 st.title('Squash It!!')
@@ -17,28 +18,28 @@ classes = [
 
 saved_fn = "predict_this.jpg"
 
-img_bytes = st.file_uploader("Squash It!!", type=['png', 'jpg', 'jpeg'])
+img_bytes = st.file_uploader("Upload a photo of your funky looking squash!", type=['png', 'jpg', 'jpeg'])
 if img_bytes is not None:
 
     st.write("Image Uploaded Successfully:")
 
-    # Get Image from Bytes, create thumbnail and save
+    # Get Image from Bytes, create thumbnail
     PILImage = PIL.Image.open(img_bytes)
     PILImage.thumbnail((500, 500))
-    PILImage.save(saved_fn)
-
-    # Convert PIL image to a FastAI Image (FastAI cannot do inference on PIL images)
-    fastai_img = open_image(saved_fn)
 
     # Display the image
     st.image(PILImage, caption="What is this gourd!?")
 
-    pred_class, pred_idx, outputs = inferencer.predict(open_image(saved_fn))
+    # Convert PIL image to a FastAI Image (FastAI cannot do inference on PIL images)
+    img_tensor = T.ToTensor()(PILImage)
+    img_fastai = Image(img_tensor)
+
+    # Make inference on image, display results:
+    pred_class, pred_idx, outputs = inferencer.predict(img_fastai)
     st.write(f"This gourd is a {pred_class} with probability {outputs[pred_idx].item():.4f}")
 
     for type, prob in zip(classes, outputs):
         st.write(f"Probability of {type}: {prob:.7f}")
 
-    os.remove(saved_fn)
 
 
